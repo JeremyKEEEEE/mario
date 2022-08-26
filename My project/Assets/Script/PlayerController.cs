db678d8201fbefa971c3d159594214e0f095beec
JeremyKEEEEE <ke.jeremy@maranathastudents.org>
2022-08-26 09:33:15 -0700

    update 1.06
    better players?

---
+++ My project/Assets/Script/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float jumpForce;
+	public bool canMove;
+	private Rigidbody2D theRB2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
