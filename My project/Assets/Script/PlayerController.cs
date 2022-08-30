d13af2c176fe13087b5bad6fae9af1a8b70dbb13
JeremyKEEEEE <ke.jeremy@maranathastudents.org>
2022-08-30 10:20:53 -0700

    update 1.07
    moving player

--- My project/Assets/Script/PlayerController.cs
+++ My project/Assets/Script/PlayerController.cs
@@ -13,12 +13,32 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        theRB2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+	{
+		canMove = true;
+	
+	
+	}
     }
+	private void FixedUpdate()
+	{
+		MovePlayer();
+	
+	}
+
+	void MovePlayer()
+	{
+		if(canMove)
+		{
+			theRB2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, theRB2D.velocity.y);
+		}
+	}
+
+
 }
